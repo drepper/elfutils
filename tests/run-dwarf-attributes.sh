f531b36edafad3df66b2ff68e8c5faf2abc1faf6
Petr Machata <pmachata@redhat.com>
2009-03-26 02:22:00 +0100

    Use newly-available --depth from dwarf-print in the test case

--- tests/run-dwarf-attributes.sh
+++ tests/run-dwarf-attributes.sh
@@ -28,8 +28,11 @@
 
 testfiles testfile
 
-testrun_compare $srcdir/dwarf-attributes.sh testfile <<\EOF
- <compile_unit offset=[0xb] stmt_list=0 high_pc=0x804845a low_pc=0x804842c name="m.c" comp_dir="/home/drepper/gnu/new-bu/build/ttt" producer="GNU C 2.96 20000731 (Red Hat Linux 7.0)" language=0x1>
+testrun_compare ./dwarf-print --depth=1 testfile <<\EOF
+testfile:
+ <compile_unit offset=[0xb] stmt_list=0 high_pc=0x804845a low_pc=0x804842c name="m.c" comp_dir="/home/drepper/gnu/new-bu/build/ttt" producer="GNU C 2.96 20000731 (Red Hat Linux 7.0)" language=0x1>...
+ <compile_unit offset=[0xca] stmt_list=0x4b high_pc=0x8048466 low_pc=0x804845c name="b.c" comp_dir="/home/drepper/gnu/new-bu/build/ttt" producer="GNU C 2.96 20000731 (Red Hat Linux 7.0)" language=0x1>...
+ <compile_unit offset=[0x15fc] stmt_list=0x1e0 high_pc=0x8048472 low_pc=0x8048468 name="f.c" comp_dir="/home/drepper/gnu/new-bu/build/ttt" producer="GNU C 2.96 20000731 (Red Hat Linux 7.0)" language=0x1>...
 EOF
 
 exit 0
