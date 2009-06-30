40c61545c91fcbdb38383c88b9a763b43bff0ba0
Roland McGrath <roland@redhat.com>
2009-06-30 12:51:57 -0700

    cruft removal

--- tests/run-dwarfcmp-self.sh
+++ tests/run-dwarfcmp-self.sh
@@ -30,9 +30,12 @@ runtest()
 {
   for file; do
     if [ -f $file ]; then
-      { testrun ../src/dwarfcmp -q -i $file $file &&
+      {
+(set -x
+       testrun ../src/dwarfcmp -q -i $file $file &&
 	testrun ../src/dwarfcmp -i $file $file &&
 	testrun ../src/dwarfcmp -T -q -i $file $file
+) > dwarfcmp-${file//\//_}.out 2>&1
       } ||
       { echo "*** failure in $file"; status=1; }
     fi
